b48483398f13817722330b4e3b055d714e01704e
Roland Smith <rsmith@xs4all.nl>
2025-08-29 00:24:29 +0200

    Redefine the buffer size. Add comments for documentation.

--- sbuf.c
+++ sbuf.c
@@ -5,9 +5,9 @@
 // Author: R.F. Smith <rsmith@xs4all.nl>
 // SPDX-License-Identifier: Unlicense
 // Created: 2025-08-28 23:49:02 +0200
-// Last modified: 2025-08-29T00:14:10+0200
+// Last modified: 2025-08-29T00:19:49+0200
 
-#include "buffer.h"
+#include "sbuf.h"
 #include <assert.h>
 #include <string.h>
 
@@ -15,7 +15,7 @@ bool sbuf_append(Sbuf *buf, const char *str, const ptrdiff_t len)
 {
   assert(buf);
   ptrdiff_t alen = strnlen(str, len);
-  ptrdiff_t remaining = PATH_MAX - buf->used - 1;
+  ptrdiff_t remaining = SBUF_MAX - buf->used - 1;
   if (len < remaining) {
     memcpy(buf->data+buf->used, str, alen);
     buf->used += alen;
@@ -27,7 +27,7 @@ bool sbuf_append(Sbuf *buf, const char *str, const ptrdiff_t len)
 ptrdiff_t sbuf_remaining(Sbuf *buf)
 {
   assert(buf);
-  ptrdiff_t remaining = PATH_MAX - buf->used - 1;
+  ptrdiff_t remaining = SBUF_MAX - buf->used - 1;
   return remaining;
 }
 
--- sbuf.h
+++ sbuf.h
@@ -5,7 +5,12 @@
 // Author: R.F. Smith <rsmith@xs4all.nl>
 // SPDX-License-Identifier: Unlicense
 // Created: 2025-08-28 23:49:02 +0200
-// Last modified: 2025-08-29T00:14:09+0200
+// Last modified: 2025-08-29T00:22:35+0200
+
+// Simple string buffer.
+// Mostly conceived for assembling strings.
+// Change the definition of SBUF_MAX if you need longer strings.
+// For general allocation, use an arena instead!
 
 #pragma once
 
@@ -14,13 +19,17 @@
 #include <stdio.h>
 #include <limits.h> // PATH_MAX
 
+#define SBUF_MAX PATH_MAX
+
 typedef struct {
   ptrdiff_t used;
-  char data[PATH_MAX];
+  char data[SBUF_MAX];
 } Sbuf;
 
-// Appends at most “len” bytes to a buffer from “str”.
+// Appends at most “len” bytes to “buf” from “str”.
 // Returns “true” if succesful, “false” if there is not enough space.
 extern bool sbuf_append(Sbuf *buf, const char *str, const ptrdiff_t len);
+// Returns how much space remains in the buffer “buf”.
 extern ptrdiff_t sbuf_remaining(Sbuf *buf);
+// Writes the buffer to the designated stream, and flushes the stream.
 extern void sbuf_fputs(Sbuf *buf, FILE* stream);
