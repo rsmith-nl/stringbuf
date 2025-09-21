ecc7746d3c16ea5e68443aceb6bac4c7036fa38f
Roland Smith <rsmith@xs4all.nl>
2025-09-21 08:33:28 +0200

    Remove assert.

--- sbuf.c
+++ sbuf.c
@@ -5,15 +5,13 @@
 // Author: R.F. Smith <rsmith@xs4all.nl>
 // SPDX-License-Identifier: Unlicense
 // Created: 2025-08-28 23:49:02 +0200
-// Last modified: 2025-09-01T22:59:43+0200
+// Last modified: 2025-09-21T08:33:11+0200
 
 #include "sbuf.h"
-#include <assert.h>
 #include <string.h>
 
 void sbuf_append(Sbuf *buf, const char *str, const ptrdiff_t len)
 {
-  assert(buf);
   if (buf->error == true) {
     return;
   }
@@ -30,21 +28,18 @@ void sbuf_append(Sbuf *buf, const char *str, const ptrdiff_t len)
 
 ptrdiff_t sbuf_remaining(Sbuf *buf)
 {
-  assert(buf);
   ptrdiff_t remaining = SBUF_MAX - buf->used - 1;
   return remaining;
 }
 
 void sbuf_fputs(Sbuf *buf, FILE* stream)
 {
-  assert(buf);
   fputs(buf->data, stream);
   fflush(stream);
 }
 
 void sbuf_reset(Sbuf *buf)
 {
-  assert(buf);
   memset(buf->data, 0, SBUF_MAX);
   buf->used = 0;
   buf->error = false;
